1cdd2b2addf6a9f40d83b3135e6f38ebf51cc77c
emilydev <emilse.a.b@gmail.com>
2023-10-23 19:59:02 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -125,7 +125,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/21710114-CF02-DU.pdf',
       },
       {
         icono: 'fas fa-download',
